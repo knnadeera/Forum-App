0cf1994123061efeac30f589182a8335391cd930
unknown <knnadeera1@gmail.com>
2022-08-04 16:01:03 +0530

    7th

--- src/components/Body/ForumContent.js
+++ src/components/Body/ForumContent.js
@@ -6,17 +6,20 @@ const ForumList = (props) => {
     <div className={classes.rap}>
       <Card>
         <div className={classes.user}>
-          <img src={props.profilepic} alt='propic' width='20' height='20' />
+          <img src={props.profilepic} alt="propic" width="20" height="20" />
           {props.user}
         </div>
         <div className={classes.forum_content}>
           <h3>{props.title}</h3>
           <p>{props.comment}</p>
         </div>
-        <div>
-          <input type="checkbox" />
-          <h8>yes</h8>
-          {props.count}
+        <div className={classes.checkbox__div}>
+          <label className={classes.checkbox}>
+            <input type="checkbox" className={classes.checkbox__input} />
+            <div className={classes.checkbox__box}></div>
+            Yes 
+          </label>
+          <div>{props.count}</div>
         </div>
       </Card>
     </div>
